8005056c27d86bdb16504361814d12fdc38514fe
celine1208 <estel2721@gmail.com>
2025-01-17 15:42:49 +0900

    20240117

---
+++ js/common.js
@@ -0,0 +1,26 @@
+$('#gnb').on('mouseenter', function(){
+    $('#header').stop().animate({height: 400}, 300)
+
+});
+
+$('#gnb').on('mouseleave', function(){
+    $('#header').stop().animate({height: 100}, 300);
+});
+
+$('#gnbList > li > a').on('mouseenter', function(){
+    $(this).addClass('active')
+           .css({borderBottom: '2px solid #B90B0B'});
+});
+
+$('#gnbList > li > a').on('mouseleave', function(){
+    $(this).removeClass('active');
+    $('#gnbList > li > a').css({borderBottom: '2px solid #fff'})
+});
+
+$('#gnbList .snb li a').on('mouseenter', function(){
+    $(this).addClass('active')
+});
+
+$('#gnbList .snb li a').on('mouseleave', function(){
+    $(this).removeClass('active');
+});
