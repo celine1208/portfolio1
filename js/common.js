076beed2c80ce1201cb9096e81a8637c596741e0
celine1208 <estel2721@gmail.com>
2025-01-21 13:52:40 +0900

    20250121

--- js/common.js
+++ js/common.js
@@ -1,13 +1,8 @@
+// 네비바 마우스 오버 이벤트
 $('#gnb').on('mouseenter', function(){
-    $('#header').stop().animate({height: 400}, 300)
-
+    $('#header').stop().animate({height: 400}, 500);
 });
 
 $('#gnb').on('mouseleave', function(){
-    $('#header').stop().animate({height: 100}, 300);
-});
-
-// 브랜드이야기 호버효과
-$('brandList li').on('onclick', function(){
-    $(this).find('div').stop().fadeIn(300);
+    $('#header').stop().animate({height: 100}, 500);
 });
