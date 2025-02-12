fd322daf0a95fd645132d7f238860e4f92c819af
celine1208 <estel2721@gmail.com>
2025-02-12 09:08:40 +0900

    fdsg

--- js/common.js
+++ js/common.js
@@ -7,20 +7,25 @@ function gnbHandler () {
 
 function resizeHandler () {
     if (window.innerWidth <= 710) {
-        $('#header').addClass('active');
+        $('#header').addClass('active')
+                   .height(60);
+    } else {
+        if (!$('#gnb a.mobileBtn').hasClass('mobileClose')) {
+            $('#header').height(100);
+        }
     }
 }
 
 // 네비바 마우스 오버 이벤트
 $('#gnb').on('mouseenter', function () {
-    if (window.innerWidth > 710) {
+    if (window.innerWidth > 1280) {
         $('#header').stop().animate({ height: 400 }, 500);
         $('#header').addClass('active');
     }
 });
 
 $('#gnb').on('mouseleave', function () {
-    if (window.innerWidth > 710) {
+    if (window.innerWidth > 1280) {
         $('#header').stop().animate({ height: 100 }, 500);
         $('body').css({ overflow: 'visible' });
         gnbHandler();
@@ -29,7 +34,7 @@ $('#gnb').on('mouseleave', function () {
 
 // 스크롤 시 네비바 배경색 변경
 $(window).on('scroll', function(){
-    if(window.innerWidth > 710){
+    if(window.innerWidth > 1280){
         $('#header').addClass('active');
         gnbHandler();
     }
@@ -46,14 +51,17 @@ $(window).on('resize', function () {
 $('#gnb a.mobileBtn').on('click', function (e) {
     e.preventDefault();
     $('.snb').hide();
-    if (window.innerWidth <= 710) {
+    if(window.innerWidth < 1280){
         $(this).toggleClass('mobilebtn mobileClose');
         if ($(this).hasClass('mobileClose')) {
             $('#header').stop().animate({ height: '100vh' }, 500);
             $('#pageTop').hide();
         }
         else {
-            $('#header').stop().animate({ height: 60 }, 500);
+            $('#header').stop().animate({ height: 100 }, 500);
+            if (window.innerWidth <= 710) {
+                $('#header').stop().animate({ height: 60 }, 500);
+            }
             $('#pageTop').show();
         }
     }
@@ -61,11 +69,14 @@ $('#gnb a.mobileBtn').on('click', function (e) {
 
 // 외부 클릭시 네비바 닫기
 $(document).on('click', function (e) {
-    if (window.innerWidth <= 710) {
+    if (window.innerWidth <= 1280) {
         // 클릭된 요소가 #gnb 내부가 아닐 경우
         if (!$(e.target).closest('#gnb').length) {
             $('#gnb a.mobileBtn').removeClass('mobileClose').addClass('mobilebtn');
-            $('#header').stop().animate({ height: 60 }, 500);
+            $('#header').stop().animate({ height: 100 }, 500);
+            if (window.innerWidth <= 710) {
+                $('#header').stop().animate({ height: 60 }, 500);
+            }
         }
     }
 });
@@ -73,7 +84,7 @@ $(document).on('click', function (e) {
 // gnbList 클릭시 서브메뉴 토글
 $('#gnbList li a').on('click', function (e) {
     e.preventDefault();
-    if (window.innerWidth <= 600) {
+    if (window.innerWidth <= 1280) {
         $(this).next().slideToggle();
         $(this).parent().siblings().children('.snb').slideUp();
     }
